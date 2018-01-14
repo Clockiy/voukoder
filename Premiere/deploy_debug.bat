6ffde5ee80e4e00a52b9026f03c8b958a8571f80
schauerland <mail@schauerland.com>
2018-01-14 19:38:29 +0100

    deploy debug to media encoder

--- Premiere/deploy_debug.bat
+++ Premiere/deploy_debug.bat
@@ -3,3 +3,4 @@
 xcopy "%1" "C:\Program Files\Adobe\Adobe Premiere Pro CS6\Plug-ins\Common\" /c /y
 xcopy "%1" "C:\Program Files\Adobe\Adobe Premiere Pro CC 2017\Plug-ins\Common\" /c /y
 xcopy "%1" "C:\Program Files\Adobe\Adobe Premiere Pro CC 2018\Plug-ins\Common\" /c /y
+xcopy "%1" "C:\Program Files\Adobe\Adobe Media Encoder CC 2018\Plug-ins\Common\" /c /y
